1e702b99b79f502404505c8a5dec1dfe11bee40e
Egor Gorbachev <egor.v.gor@gmail.com>
2024-11-17 08:49:37 +0300

    Version 1.0

---
+++ core/src/main/java/ru/gopher/Main.java
@@ -0,0 +1,42 @@
+package ru.gopher;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    public static final float SCREEN_WIDTH = 1280;
+    public static final float SCREEN_HEIGHT = 720;
+    private SpriteBatch batch;
+    private Texture image;
+    private float x = 0, y = 0;
+    private float stepX = 7, stepY = 4;
+    private final float IMG_WIDTH = 378, IMG_HEIGHT = 63;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void render() {
+        x += stepX;
+        y += stepY;
+        if (x > SCREEN_WIDTH - IMG_WIDTH || x < 0) stepX = -stepX;
+        if (y > SCREEN_HEIGHT - IMG_HEIGHT || y < 0) stepY = -stepY;
+
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(image, x, y);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
